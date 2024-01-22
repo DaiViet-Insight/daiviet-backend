1dc95a73797c3d4b919451eec3b7df47f0b7a618
tranhuutuan261103 <tranhuutuan26112003@gmail.com>
2024-01-22 20:45:44 +0700

    first commit

---
+++ src/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+const morgan = require('morgan');
+app.use(morgan('dev'));
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const router = express.Router();
+
+// CORS error handling
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // '*' for all
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if (req.method == 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET'); // '*' for all
+        return res.status(200).json({});
+    }
+    next();
+});
+app.use('/', router);
+
+router.get('/', (req, res, next) => {
+    res.send('Server is running ...');
+});
+
+module.exports = app;
