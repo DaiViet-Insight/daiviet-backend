405b655b4d92fbf94949889b0ea3b28dbf9b0e1d
Ubuntu <melicom@melicom.wqi4a44i2t0uxmkwlwd5ip2uag.ex.internal.cloudapp.net>
2024-01-31 02:25:35 +0000

    modified

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -11,15 +11,7 @@ module.exports = {
         const postId = req.query.afterPost;
         const limit = parseInt(req.query.size);
         const eventId = req.query.eventId;
-        let userId = req.userId;
-        if (!userId) {
-            if(req.session.token != null) {
-                userId = jwtService.decodeToken(req.session.token).id;
-            }
-            else {
-                res.status(500).json({ error: "Lỗi không có userID" });
-            }
-        }
+        let userId = req.userId;    
         try {
             let posts;
             if (type === "new") {
@@ -36,13 +28,24 @@ module.exports = {
                     postId,
                     eventId
                 );
-            } else if (status === "upvoted") {
-                posts = await PostVoteService.getPostsByUserId(userId, 1);
-            } else if (status === "downvoted") {
-                posts = await PostVoteService.getPostsByUserId(userId, 2);
-            } else if (status === "saved") {
-                posts = await PostSaveService.getPostsByUserId(userId);
-            }
+            } else {
+                if (!userId) {
+                    if(req.session.token != null) {
+                        userId = jwtService.decodeToken(req.session.token).id;
+                    }
+                    else {
+                        res.status(500).json({ error: "Lỗi không có userID" });
+                        return;
+                    }
+                }
+                if (status === "upvoted") {
+                    posts = await PostVoteService.getPostsByUserId(userId, 1);
+                } else if (status === "downvoted") {
+                    posts = await PostVoteService.getPostsByUserId(userId, 2);
+                } else if (status === "saved") {
+                    posts = await PostSaveService.getPostsByUserId(userId);
+                }
+            } 
             res.send(posts);
         } catch (error) {
             res.status(500).json({ error: error.message });
